5a61d0fce0db2fd4f4a430446088cf8da1fca548
Oleg <kolyushko.oleg@gmail.com>
2020-11-06 20:39:06 +0200

    Updates

---
+++ precache-manifest.fdcef5d0b76e4622be5ae47467791659.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa88f7a6eb918e8883aca42e7f2bc8de",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "dfecf363b9111312fdaa",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eebd7ef82772f1accbf",
+    "url": "/goit-react-hw-02-feedback/static/js/main.0e25c1c3.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
